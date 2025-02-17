7082a1a964c4dad97cd8f9f653ae3c183004dd28
LAPTOP-3123UUO5\jeral <jerald.timbang@veent.com>
2025-02-17 15:40:21 +0800

    Jerald - first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Create from './templetes/create.svelte';
+  import RegistrationConfirmation from './templetes/RegistrationConfirmation.svelte'; 
+
+  let isRegistered = false;
+let registrationData = {
+  eventName: '',
+  subdomain: '',
+  startDate: '',
+  startTime: '',
+  endDate: '',
+  endTime: '',
+  location: '',
+  description: '',
+  companyLogo: null,
+  poster: null,
+  backgroundImage: null
+};
+
+function handleRegistration(event: CustomEvent) {
+  registrationData = event.detail;
+  isRegistered = true;
+}
+</script>
+
+{#if !isRegistered}
+<Create on:submit={handleRegistration} />
+{:else}
+<RegistrationConfirmation {registrationData} />
+{/if}
